fbbb47029549e66eca52e3f2fc4e2047c2b68807
Andrew Demonov <fcoder@live.com>
2017-10-02 02:10:05 -0400

    InlineKeyboard support

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,64 @@
 extern crate teleborg;
-use teleborg::{Dispatcher, Bot, Updater};
-use teleborg::objects::Update;
+use teleborg::{Dispatcher, Bot, Updater, NO_MARKUP};
+use teleborg::objects::{InlineKeyboardButton, InlineKeyboardMarkup, Message, Update};
 
 fn main() {
-    let bot_token = "token".to_string();
     let mut dispatcher = Dispatcher::new();
     
     dispatcher.add_message_handler(msg_handler_fn);
     dispatcher.add_command_handler("debug", cmd_debug_fn, false);
     
-    Updater::start(Some(bot_token), None, None, None, dispatcher);
+    Updater::start(None, None, None, None, dispatcher);
 }
 
 fn cmd_debug_fn(bot: &Bot, update: Update, args: Option<Vec<&str>>) {
     let text = format!("debug\n{:?}", args);
     if let Err(e) = bot.reply_to_message(&update, &text) {
         println!("Couldn't send debug message: {:?}", e);
-    }   
+    }
 }
 
 fn msg_handler_fn(bot: &Bot, update: Update, args: Option<Vec<&str>>) {
-    match parse_message(&update) {
-        Err(e) => println!("{}", e),
+    if let Some(ref callback_query) = update.callback_query {
+        match parse_message(&callback_query.message) {
+            Err(e) => println!("{}", e),
+            Ok((chat_id, req_text)) => {
+                if let Some(ref res_text) = callback_query.data {
+                    if let Err(e) = bot.send_message(&chat_id, &res_text, None, None, None, None, NO_MARKUP) {
+                        println!("Couldn't send message: {:?}", e);
+                    }
+                } else {
+                    println!("Couldn't parse callback_query data");
+                }
+            }
+        }
+    } else {
+        match parse_message(&update.message) {
+            Err(e) => println!("{}", e),
+            Ok((chat_id, req_text)) => {
+                let res_text = format!("{} -> response русский текст\n{:?}", req_text, args);
 
-        Ok((chat_id, req_text)) => {
-            let res_text = format!("{} -> response русский текст\n{:?}", req_text, args);
-            if let Err(e) = bot.send_message(&chat_id, &res_text, None, None, None, None, None) {
-                println!("Couldn't send message: {:?}", e);
+                let button = InlineKeyboardButton::new("Title".to_string(), None, Some(String::from("abc")), None, None);
+                let markup = InlineKeyboardMarkup::new(vec![vec![button]]);
+
+                if let Err(e) = bot.send_message(&chat_id, &res_text, None, None, None, None, Some(markup)) {
+                    println!("Couldn't send message: {:?}", e);
+                }
+
             }
         }
     }
 }
 
-fn parse_message(update: &Update) -> Result<(i64, &str), &str> {
-    match update.message {
-        Some(ref message) => {
-            let text = match message.text {
-                Some(ref s) => &s,
+fn parse_message<'a>(msg:&'a Option<Message>) -> Result<(i64, &'a str), &'a str> {
+    if let &Some(ref message) = msg {
+        let text = match message.text {
+                Some(ref s) => s,
                 None => "",
-            };
+        };
 
-            Ok((message.chat.id, text))
-        },
-        None => Err("message not found"),
-    }    
+        return Ok((message.chat.id, text))
+    }
+    
+    Err("message not found")
 }
